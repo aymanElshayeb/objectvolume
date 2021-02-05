50842690995c168fcd740c70451a094ea32de3bd
ayman elshayeb <ayman.elshayb@gmail.com>
2021-02-05 20:10:01 +0100

    first commit

---
+++ src/main/java/com/itambition/www/objectvolume/ObjectvolumeApplication.java
@@ -0,0 +1,13 @@
+package com.itambition.www.objectvolume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjectvolumeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObjectvolumeApplication.class, args);
+	}
+
+}
